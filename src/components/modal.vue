63a6afd4b2babc8218fdca416f03ed8909d84f42
Amit Gurbani <amit1994.gurbani@gmail.com>
2020-12-28 00:23:24 +0530

    Added open and close modal

---
+++ src/components/modal.vue
@@ -0,0 +1,35 @@
+<template>
+  <ion-header>
+    <ion-toolbar>
+      <ion-title>Modal</ion-title>
+    </ion-toolbar>
+  </ion-header>
+  <ion-content class="ion-padding">
+    This is a modal <br />
+    <ion-button @click="closeModal">Close Modal</ion-button>
+  </ion-content>
+</template>
+
+<script>
+import {
+  IonContent,
+  IonHeader,
+  IonTitle,
+  IonToolbar,
+  IonButton,
+  modalController,
+} from "@ionic/vue";
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Modal",
+  components: { IonContent, IonHeader, IonTitle, IonToolbar, IonButton },
+  setup() {
+    const closeModal = () => {
+      modalController.dismiss();
+    };
+
+    return { closeModal };
+  },
+});
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,67 +2,49 @@
   <ion-page>
     <ion-header :translucent="true">
       <ion-toolbar>
-        <ion-title>Blank</ion-title>
+        <ion-title>Ionic Vue Modal Example</ion-title>
       </ion-toolbar>
     </ion-header>
-    
+
     <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Blank</ion-title>
-        </ion-toolbar>
-      </ion-header>
-    
-      <div id="container">
-        <strong>Ready to create an app?</strong>
-        <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
-      </div>
+      <ion-button @click="openModal">Open Modal</ion-button>
     </ion-content>
   </ion-page>
 </template>
 
 <script lang="ts">
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
-import { defineComponent } from 'vue';
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonButton,
+  modalController,
+} from "@ionic/vue";
+import { defineComponent } from "vue";
+
+import Modal from "@/components/modal.vue";
 
 export default defineComponent({
-  name: 'Home',
+  name: "Home",
   components: {
     IonContent,
     IonHeader,
     IonPage,
     IonTitle,
-    IonToolbar
-  }
+    IonToolbar,
+    IonButton,
+  },
+  setup() {
+    const openModal = async () => {
+      const modal = await modalController.create({
+        component: Modal, //Modal is name of the component to render inside ionic modal
+      });
+      return modal.present();
+    };
+
+    return { openModal };
+  },
 });
 </script>
-
-<style scoped>
-#container {
-  text-align: center;
-  
-  position: absolute;
-  left: 0;
-  right: 0;
-  top: 50%;
-  transform: translateY(-50%);
-}
-
-#container strong {
-  font-size: 20px;
-  line-height: 26px;
-}
-
-#container p {
-  font-size: 16px;
-  line-height: 22px;
-  
-  color: #8c8c8c;
-  
-  margin: 0;
-}
-
-#container a {
-  text-decoration: none;
-}
-</style>
